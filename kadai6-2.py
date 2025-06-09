804908b0607dac8058c7f8192ac5d636efc86701
j23257滝澤一路 <j23257ti@edu.tuis.ac.jp>
2025-06-09 11:26:07 +0900

    update: kadai6-2.pyを一部変更()

--- kadai6-2.py
+++ kadai6-2.py
@@ -1,5 +1,5 @@
 import requests
-API_URL  = "https://api.data.metro.tokyo.lg.jp/v1/Covid19Patient?from=2020-04-01&till=2020-04-30&limit=100" #取得するAPIのURL(e-stat)
+API_URL  = "https://api.data.metro.tokyo.lg.jp/v1/Covid19Patient?from=2020-04-01&till=2020-04-30&limit=100" #取得するAPIのURL(TOKYO OPEN DATA)
 #新型コロナウイルス陽性患者発表詳細(2020年4月1日～2020年4月30日まで)　に関するAPI_URL
 
 #東京都オープンデータカタログサイトより引用
